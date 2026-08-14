f526bd96af903a91834f97499bb54b262fd06694
Kyle San Clemente <kylesanclemente@gmail.com>
2026-08-14 16:41:50 -0700

    v0.21.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Severance.MixProject do
   def project do
     [
       app: :severance,
-      version: "0.20.0",
+      version: "0.21.0",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
